4d6db94b8ebeef65fd4af4ec72cc37887d275991
finnvisz <finndokter@outlook.com>
2024-06-04 15:43:09 +0200

    jkbsmnb

--- railnl.py
+++ railnl.py
@@ -12,6 +12,8 @@ class Railnl:
                         name, x, y = line.strip().split(',')
                         station = Station(name, x, y)
                         self.stations[name] = station
+        print(self.stations)
+
 
     def load_connections(self) -> None:
         with open("data/ConnectiesHolland.csv") as f:
@@ -19,6 +21,20 @@ class Railnl:
                         if line == "\n":
                             break
                         station1, station2, afstand = line.strip().split(',')
-                        station1.add_connection(station2, afstand)
+                        self.stations[station1].add_connection(self.stations[station2], afstand)
+                        self.stations[station2].add_connection(self.stations[station1], afstand)
 
-    # Git commit oefen
+    def max_connections(self) -> int:
+        print(self.stations.keys())
+        return max(self.stations[station].number_of_connections() for station in self.stations.keys())
+
+
+if __name__ == "__main__":
+    railnl = Railnl()
+    railnl.load_connections()
+    railnl.load_stations()
+    print(railnl.max_connections())
+            
+          
+
+    
--- station.py
+++ station.py
@@ -3,7 +3,11 @@ class Station:
         self.name = name
         self.x = x
         self.y = y
-        self.connections: dict[str, int] = {}
+        self.connections: dict["Station", int] = {}
 
     def add_connection(self, other: "Station", afstand: int) -> None:
         self.connections[other] = afstand
+
+    def number_of_connections(self) -> int:
+        return len(self.connections)
+
