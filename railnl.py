d8e2f4b92ef8ae2eb3d7f99aea6302e2a38ac826
finnvisz <finndokter@outlook.com>
2024-06-04 11:20:28 +0200

    data ophalen

---
+++ railnl.py
@@ -0,0 +1,25 @@
+from station import Station
+
+class Railnl:
+    def __init__(self) -> None:
+        self.stations: dict[str, "Station"] = {}
+    
+    def load_stations(self) -> None:
+        with open("data/StationsHolland.csv") as f:
+                    for line in f:
+                        if line == "\n":
+                            break
+                        name, x, y = line.strip().split(',')
+                        station = Station(name, x, y)
+                        self.stations[name] = station
+
+
+    def load_connections(self) -> None:
+        with open("data/ConnectiesHolland.csv") as f:
+                    for line in f:
+                        if line == "\n":
+                            break
+                        station1, station2, afstand = line.strip().split(',')
+                        station1.add_connection(station2, afstand)
+
+
---
+++ station.py
@@ -0,0 +1,9 @@
+class Station:
+    def __init__(self, name: str, x: float, y: float):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.connections: dict[str, int] = {}
+
+    def add_connection(self, other: "Station", afstand: int) -> None:
+        self.connections[other] = afstand
