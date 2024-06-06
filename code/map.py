00f4174adf58b9789b1444b8cf4c5af00b1099a5
tije-schut <tije.c.schut@gmail.com>
2024-06-06 15:56:16 +0200

    map completion

--- code/map.py
+++ code/map.py
@@ -19,8 +19,8 @@ class Map(MovingCameraScene):
 
             # Create dot from station object location 
             lat, long = object.location()
-            dot = Dot(point = lat * LEFT + long * UP, color = WHITE)
-            dot.scale(0.1)
+            position = lat * LEFT + long * UP
+            dot = Dot(point = position, radius = 0.008)
 
             # Add station dot pair to dict
             self.dot_dictionary[object] = dot
@@ -35,26 +35,23 @@ class Map(MovingCameraScene):
 
     def create_connections(self):
         self.station_connections = VGroup()
-
         for station, dot_start in self.dot_dictionary.items():
             for connecting in station.connecting_stations():
                 dot_end = self.dot_dictionary[connecting]
-                line = Line(dot_start.get_center(), dot_end.get_center())
-                line.scale(0.1)
+                line = Line(start = dot_start.get_center(), end = dot_end.get_center(), stroke_width = 0.25)
                 self.station_connections.add(line)
 
     def construct(self):
     
         self.create_points_labels()
         self.create_connections()
-        print(self.create_connections())
 
         # Set camera to instantiated points and labels
         self.camera.frame.move_to(self.station_points)
-        self.camera.frame.set_height(1.25 * self.station_points.get_height())
+        self.camera.frame.set_height(1.25 * self.station_points.height)
     
         # Fade labels in, transform into points
         self.play(FadeIn(self.station_labels), run_time = 2)
-        self.play(Transform(self.station_labels, self.station_points))
-        self.play(Transform(self.station_points, self.station_connections))
-        self.wait(5)
+        self.play(Transform(self.station_labels, self.station_points), run_time = 2)
+        self.play(FadeIn(self.station_connections), run_time = 2)
+        self.wait(2)
