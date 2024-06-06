9f74d792e9394c9820811acaf1713438f97ea2f1
finnvisz <finndokter@outlook.com>
2024-06-06 15:23:04 +0200

    score and random_algorithm classes

--- code/score.py
+++ code/score.py
@@ -1,18 +1,28 @@
+from random_algorithm import RandomAlgorithm
+
 class Score:
     def __init__(self) -> None:
+        self.algorithm = RandomAlgorithm()
+        self.algorithm.make_trajects()  # Ensure trajects are generated
         self.Min = 0
         self.T = 0
         self.total_connections_used = set()
 
-        for traject in self.trajects:
+    def calculate(self) -> float:
+        for traject in self.algorithm.trajects:
             self.Min += traject.time
-            for con in tra.connections_used:
+            for con in traject.connections_used:
                 # Ensure that each connection is a tuple
-                total_connections_used.add(tuple(con))
-            if tra.time != 0:
-                T += 1
+                self.total_connections_used.add(tuple(con))
+            if traject.time != 0:
+                self.T += 1
 
-        tot_connections_available = set(self.load.connections)
-        p = len(total_connections_used) / len(tot_connections_available)
-        K = p * 10000 - (T * 100 + Min)
+        tot_connections_available = set(map(tuple, self.algorithm.data.connections))
+        print(tot_connections_available)
+        p = len(self.total_connections_used) / len(tot_connections_available)
+        K = p * 10000 - (self.T * 100 + self.Min)
         return K
+
+score = Score()
+print(score.calculate())
+
