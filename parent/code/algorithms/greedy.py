5666302637c4bbbf309a66f528fc6d553f73f864
finnvisz <finndokter@outlook.com>
2024-06-10 17:53:37 +0200

    i forgot

--- parent/code/algorithms/greedy.py
+++ parent/code/algorithms/greedy.py
@@ -12,7 +12,7 @@ class Greedy(Algorithm):
         super().__init__(load)
 
     def run(self):
-        self.routes = []  # Ensure routes are clear before running
+        self.routes = [] 
         while self.number_of_routes() < 7 and len(self.get_connections_used()) < len(set(map(tuple, self.load.connections))):
             time_used = 0
             current_station = random.choice(list(self.load.stations_dictionary().values()))
@@ -20,27 +20,8 @@ class Greedy(Algorithm):
             visited_connections = set()
 
             while current_station.has_connections():
-                connections = [
-                    (conn, int(current_station.connection_duration(conn)))
-                    for conn in current_station.connecting_stations()
-                    if (current_station, conn) not in visited_connections and (conn, current_station) not in visited_connections
-                ]
-
-                if not connections:
-                    break
-
                 # Select the connection with the shortest duration
                 connection, duration = min(connections, key=lambda x: x[1])
-                total_time = time_used + duration
-
-                if total_time <= 120:
-                    time_used = total_time
-                    route.add(current_station, connection, duration)
-                    visited_connections.add((current_station, connection))
-                    visited_connections.add((connection, current_station))
-                    current_station = connection
-                else:
-                    break
-
+                
             self.routes.append(route)
             
