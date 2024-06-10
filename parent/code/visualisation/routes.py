f4049912d48c2385df29c9667da00e5e61d00cf1
tije-schut <tije.c.schut@gmail.com>
2024-06-10 19:07:27 +0200

    visualisations

--- parent/code/visualisation/routes.py
+++ parent/code/visualisation/routes.py
@@ -1,14 +1,18 @@
 from parent.code.visualisation.map import Map
 from parent.code.classes.route import Route
-from manim import Create, MovingCameraScene
+from manim import RED, Transform
 
 class Display_Route(Map):
     """Show trainroutes on map."""
 
-    def color_route(self, route: Route):
-        pass
+    def color_route(self, route: list):
+        
+        for connection in list:
+            line = self.traject_line_dict[connection]
+            line.set_color(RED)
 
     def construct(self):
         self.add(self.points, self.connections)
+        self.color_route()
         self.wait(4)
         
