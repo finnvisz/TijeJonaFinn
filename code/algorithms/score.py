6cba43790ed7b0807aabc796bbf3703038bfc383
finnvisz <finndokter@outlook.com>
2024-06-10 11:17:25 +0200

    route ipv traject

--- code/algorithms/score.py
+++ code/algorithms/score.py
@@ -10,12 +10,12 @@ class Score:
         self.total_connections_used = set()
 
     def calculate(self) -> float:
-        for traject in self.algorithm.trajects:
-            self.Min += traject.time
-            for con in traject.connections_used:
+        for route in self.algorithm.routes:
+            self.Min += route.time
+            for con in route.connections_used:
                 # Ensure that each connection is a tuple
                 self.total_connections_used.add(tuple(con))
-            if traject.time != 0:
+            if route.time != 0:
                 self.T += 1
 
         tot_connections_available = set(map(tuple, self.algorithm.load.connections))
