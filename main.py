922af17ea59fad19dc0b243e3ce2137c83895e40
tije-schut <tije.c.schut@gmail.com>
2024-06-10 13:25:36 +0200

    main script manim

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from code.algorithms.algorithm import Algorithm
 from code.algorithms.random_algorithm import RandomAlgorithm
 from code.algorithms.score import Score
 from code.classes.station_class import Station
-
+from code.visualisation.map import Map
+from subprocess import run
 
 if __name__ == "__main__":
     data = RailNL("Holland")
@@ -11,3 +12,14 @@ if __name__ == "__main__":
     algorithm.make_picture()
     score = Score(algorithm)
     print(score.calculate())
+
+# if __name__ == "__main__":
+#     data = RailNL("Holland")
+#     command = [
+#         "manim",
+#         "-pql",
+#         "code/visualisation/map.py",
+#         "Map(data)"
+#     ]
+
+#     run(command)
