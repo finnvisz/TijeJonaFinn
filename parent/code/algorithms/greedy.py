96ef2cae895f3f71d65d1bfbfff1d00b53c49340
Jona Aalten <jona.aalten@student.uva.nl>
2024-06-12 15:33:54 +0200

    bugfix: set_as_used in greedy algoritme

--- parent/code/algorithms/greedy.py
+++ parent/code/algorithms/greedy.py
@@ -24,11 +24,16 @@ class Greedy(Algorithm):
         
         # unused_connections starts off with all connections
         # Unused connections will be moved to used connections when they are used
-        self.unused_connections: set[tuple] = copy.deepcopy(self.load.connections)
-        self.used_connections: set  = set()
+        self.used_connections: dict = dict()
+        
+        self.unused_connections: dict = dict()
+        for connection in self.load.connections:
+            # Save the names of the stations in this connection in a tuple
+            station_names_as_tuple = tuple(sorted([connection[0].name, connection[1].name]))
 
-        # DEBUG
-        print(self.unused_connections)
+            # Add the connection to the dict with the station name tuple as key
+            self.unused_connections[station_names_as_tuple] = connection
+
 
         # While there are less than 7 routes and there are still unused connections
         while self.number_of_routes() < 7 and len(self.unused_connections) > 0:
@@ -39,9 +44,13 @@ class Greedy(Algorithm):
             current_station = self.load.get_random_station()
 
             # While time is less than 120 minutes
-            while route.time < 120:
+            while route.time < 120: 
                 # Select the connection with the shortest duration
                 next_station: "Station" = current_station.get_shortest_connection()
+
+                # DEBUG
+                print(f"Current station: {current_station.name}")
+                print(f"Next station: {next_station.name}")
 
                 # Add the connection to the route
                 route.add(current_station, next_station, current_station.get_connection_time(next_station))
@@ -57,23 +66,21 @@ class Greedy(Algorithm):
         # Return the generated routes
         return self.routes
 
-    def set_as_used(self, current_station: "Station", next_station: "Station"):
+    def set_as_used(self, current_station: "Station", next_station: "Station") -> None:
         """
         Takes two stations and moves the connection between them from unused to used connections.
         Order of the stations does not matter, connections are handled alphabetically.
         """
-        # Sort the connection alphabetically and create tuple
-        connection_sorted = tuple(sorted([current_station, next_station], key=lambda x: x.name))
-        
-        # Move the connection from unused to used connections
-        self.unused_connections.remove(connection_sorted)
-        print(f"Connection {connection_sorted} has been used.")
-        self.used_connections.add(connection_sorted)
+        # Extract dictionary key for the connection
+        connection_key = tuple(sorted([current_station.name, next_station.name]))
+
+        # If key is currently set as unused: set as used
+        if connection_key in self.unused_connections:
+            # Pop this key-value pair from unused and add to the used connections
+            self.used_connections[connection_key] = self.unused_connections.pop(connection_key)
+
 
 if __name__ == "__main__":
     greedy = Greedy(RailNL("Holland"))
-    
-    
-    
     output = greedy.run()
     print(output)
