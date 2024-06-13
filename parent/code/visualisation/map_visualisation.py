27c0ff694a08c132f18b2f072f44de012c328543
tije-schut <tije.c.schut@gmail.com>
2024-06-13 13:53:54 +0200

    train

--- parent/code/visualisation/map_visualisation.py
+++ parent/code/visualisation/map_visualisation.py
@@ -3,23 +3,27 @@ from base_map import Map
 
 class map_visualisation(MovingCameraScene):
 
+    def setup(self):
+        self.setup_map()
+        self.setup_camera()
+
+    # Get all relevant objects from base_map
+    def setup_map(self):
+        map = Map()
+        self.dots = map.give_dots()
+        self.dot_labels = map.give_dot_labels()
+        self.connections = map.give_connections()
+        self.connection_labels = map.give_connection_labels()
+
+    # Move camera to capture network
+    def setup_camera(self):
+        self.camera.frame.move_to(self.dots)
+        self.camera.frame.set_height(1.1 * self.dots.get_height())
+
     # Play scene showing labels, points and connections
     def construct(self):
-    
-        # Get all objects from base_map
-        map = Map()
-        dots = map.give_dots()
-        dot_labels = map.give_dot_labels()
-        connections = map.give_connections()
-        connection_labels = map.give_connection_labels()
-
-        # Move camera to capture network
-        self.camera.frame.move_to(dots)
-        self.camera.frame.set_height(1.1 * dots.get_height())
-
-        # Fade labels in, transform into points
-        self.play(FadeIn(dot_labels), run_time = 2)
-        self.play(Transform(dot_labels, dots), run_time = 2)
-        self.play(FadeIn(connections), run_time = 2)
-        self.play(FadeIn(connection_labels), run_time = 2)
+        self.play(FadeIn(self.dot_labels), run_time = 2)
+        self.play(Transform(self.dot_labels, self.dots), run_time = 2)
+        self.play(FadeIn(self.connections), run_time = 2)
+        self.play(FadeIn(self.connection_labels), run_time = 2)
         self.wait(2)
--- parent/code/visualisation/route_visualisation.py
+++ parent/code/visualisation/route_visualisation.py
@@ -1,5 +1,6 @@
-from manim import MovingCameraScene, ApplyMethod, Line, manim_colors
-from manim import PURE_RED, PURE_GREEN, PURE_BLUE, YELLOW, PURPLE, ORANGE, PINK
+from manim import MovingCameraScene, ApplyMethod, Line, manim_colors, Dot
+from manim import PURE_RED, PURE_GREEN, PURE_BLUE
+from manim import YELLOW, PURPLE, ORANGE, PINK
 from parent.code.classes.railnl import RailNL
 from parent.code.algorithms.random_algorithm import RandomAlgorithm
 from base_map import Map
@@ -8,10 +9,38 @@ class route_visualisation(MovingCameraScene):
     """Show trainroutes on map."""
 
     def setup(self):
-        self.map = Map()
+        self.setup_map()
+        self.setup_camera()
+        self.run_algorithm()
+
         self.line_colors_dict: dict[Line, list] = {}
-        self.colors = [PURE_RED, PURE_GREEN, PURE_BLUE, YELLOW, 
+        self.colors = [PURE_RED, PURE_GREEN, 
+                       PURE_BLUE, YELLOW, 
                        PURPLE, ORANGE, PINK]
+        
+    # Get all relevant objects from base_map
+    def setup_map(self):
+        self.map = Map()
+        self.dots = self.map.give_dots()
+        self.dot_labels = self.map.give_dot_labels()
+        self.connections = self.map.give_connections()
+        self.connection_labels = self.map.give_connection_labels()
+
+    # Move camera to capture network
+    def setup_camera(self):
+        self.camera.frame.move_to(self.dots)
+        self.camera.frame.set_height(1.1 * self.dots.get_height())
+
+    # Make train to ride tracks
+    def setup_train(self, dot_start: Dot):
+        self.train = Dot(point = dot_start.get_center(), radius = 0.0075)
+
+    # Run algorithm here
+    def run_algorithm(self):
+        data = RailNL("Holland")
+        algorithm = RandomAlgorithm(data) # HERE
+        algorithm.run()
+        self.dienstregeling = algorithm.output()
 
     def color_route(self, route: list, color: manim_colors):
 
@@ -20,6 +49,7 @@ class route_visualisation(MovingCameraScene):
         station_dot_dict = self.map.give_station_dot_dict()
         connection_line_dict = self.map.give_connection_line_dict()
 
+        start_route = 0
         for connection in route:
             time = connection[2]
 
@@ -31,17 +61,23 @@ class route_visualisation(MovingCameraScene):
             dot_start = station_dot_dict[station_start]
             dot_end = station_dot_dict[station_end]
 
+            if start_route == 0:
+                self.setup_train(dot_start)
+                start_route += 1
+
             # Check if connection tuple exists
             tuple = (dot_start, dot_end, time)
             if tuple in connection_line_dict:
                 line = connection_line_dict[tuple]
-                self.set_color(line, color)
             
             # Otherwise search reversed connection
             else:
                 tuple = (dot_end, dot_start, time)
                 line = connection_line_dict[tuple]
-                self.set_color(line, color)
+            
+            self.set_color(line, color)
+            shift = self.train.animate.move_to(dot_end.get_center())
+            self.play(shift, run_time = 0.5)
 
     # Set color depending on if already colored.
     def set_color(self, line: Line, color: manim_colors):
@@ -49,7 +85,7 @@ class route_visualisation(MovingCameraScene):
         if not line in self.line_colors_dict:
 
             # If line not yet colored just color
-            self.play(ApplyMethod(line.set_color, color))
+            self.play(ApplyMethod(line.set_color, color), run_time = 0.1)
             self.line_colors_dict[line] = [color]
 
         else:
@@ -62,29 +98,16 @@ class route_visualisation(MovingCameraScene):
                 # Otherwise update line gradient
                 colors = self.line_colors_dict[line]
                 colors.append(color)
-                self.play(ApplyMethod(line.set_color, colors))
+                self.play(ApplyMethod(line.set_color, colors), run_time = 0.1)
 
     def construct(self):
-        
-        # Get all objects from base_map
-        dots = self.map.give_dots()
-        connections = self.map.give_connections()
-        connection_labels = self.map.give_connection_labels()
 
-        # Move camera to capture network
-        self.camera.frame.move_to(dots)
-        self.camera.frame.set_height(1.1 * dots.get_height())
-
-        self.add(dots, connections, connection_labels)
+        self.add(self.dots, self.connections, self.connection_labels)
         self.wait(2)
 
-        data = RailNL("Holland")
-        random = RandomAlgorithm(data)
-        random.run()
-        dienstregeling = random.output()
-
         for count in range(7):
-            route = dienstregeling[count].connections()
+            route_object = self.dienstregeling[count]
+            route = route_object.connections()
             self.color_route(route, self.colors[count])
 
         self.wait(2)
