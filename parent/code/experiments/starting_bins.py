1139e96bff84fd2f5241db69552eaa6f514e6faf
tije-schut <tije.c.schut@gmail.com>
2024-06-20 15:57:18 +0200

    results directory

--- parent/code/experiments/starting_bins.py
+++ parent/code/experiments/starting_bins.py
@@ -8,21 +8,23 @@ class Sort_Starting():
     def __init__(self):
         railnl = RailNL("Holland")
         self.station_list = railnl.stations_dict().values()
-        self.bins = [[] for _ in range(12, 25)]
+        self.bins: dict[int, list[Station]] = {i: [] for i in range(12, 25)}
         
         self.fill_bins()
+        print(self.bins)
 
     def fill_bins(self):
 
-        for combination in combinations(self.station_list, 2):
+        # For each non-repeating combination of 7 stations
+        for combination in combinations(self.station_list, 7):
 
+            # Calculate connectivity sum
             amount = 0
             for station in combination:
                 amount += station.amount_connecting()
-            
-            print(amount)
-            print(combination)
 
-            self.bins[amount].append(combination)
+            # Place in correct connectivity degree bin
+            self.bins[amount].append(list(combination))
 
-Sort_Starting()
+    def give_connectivity_bin_dict(self):
+        return self.bins
