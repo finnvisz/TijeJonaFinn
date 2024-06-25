c791ec03fcf2dbbac111ea1e950665954130b296
finnvisz <finndokter@outlook.com>
2024-06-25 16:20:18 +0200

    clean up RailNL

--- parent/code/classes/railnl.py
+++ parent/code/classes/railnl.py
@@ -8,7 +8,13 @@ class RailNL:
     """Class containing all stations and their connections."""
 
     def __init__(self, maprange: str = "Holland") -> None:
-        """Creates Railnl object, loads stations and connections into it."""
+        """
+        Initialize a RailNL object with stations and connections.
+        
+        - Pre: maprange is either "Holland" or "Nationaal".
+        - Post: self.stations contains all stations and their connections 
+          from the corresponding CSV files based on the maprange.
+        """
         self.mapname = maprange
         
         self.stations: dict[str, "Station"] = {}
@@ -18,7 +24,8 @@ class RailNL:
         
     
     def load_stations(self, filepath: str) -> None:
-        """Load stations from data file into self.stations.
+        """
+        Load stations from data file into self.stations.
         
         - Pre: filepath is a valid path to a CSV file containing station data.
         CSV file is formatted as follows:
@@ -39,9 +46,10 @@ class RailNL:
                 # And add to internal dictionary
                 self.stations[name] = station
 
-    # Add connections to Station objects in self.stations
+
     def load_connections(self, filepath: str) -> None:
-        """Load connections from data file into the stations of self.stations.
+        """
+        Load connections from data file into the stations of self.stations.
         
         - Pre: filepath is a valid path to a CSV file containing connection data.
         CSV file is formatted as follows:
@@ -72,28 +80,55 @@ class RailNL:
                 # add the connections to this class
                 self.connections.add((stat1_o, stat2_o))
 
+
     def stations_dict(self) -> dict:
+        """
+        Return a dictionary of stations.
+        
+        - Post: Returns a dictionary where keys are station names and values 
+          are Station objects. (used to get station objects using the name)
+        """
         return self.stations
 
-    # Find the highest number of station connections
+
     def max_connections(self) -> int:
+        """
+        Find the highest number of connections of any station.
+        
+        - Post: Returns the maximum number of connections any station has.
+        """
         stations = self.stations.keys()
 
         # Find amount of connections using amount_connecting method
         connections = lambda station: self.stations[station].amount_connecting()
         return max(connections(station) for station in stations)
     
-    # Find the lowest number of station connections
+
     def min_connections(self) -> int:
+        """
+        Find the lowest number of connections of any station.
+        
+        - Post: Returns the minimum number of connections any station has.
+        """
         stations = self.stations.keys()
 
         # Find amount of connections using amount_connecting method
         connections = lambda station: self.stations[station].amount_connecting()
         return min(connections(station) for station in stations)
     
+
     def get_random_station(self) -> "Station":
-        """Return a random station from self.stations."""
+        """
+        Return a random station from self.stations.
+        """
         return choice(list(self.stations.values()))
     
+
     def get_total_connections(self) -> set:
+        """
+        Return a set of all connections.
+        
+        - Post: Returns a set of tuples, where each tuple contains two 
+          connected Station objects.
+        """
         return set(map(tuple, self.connections))
