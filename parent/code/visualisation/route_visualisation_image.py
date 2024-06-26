0c6ccd16fbdda6b2356e4dfce3aebb6239a36241
tije-schut <tije.c.schut@gmail.com>
2024-06-26 13:26:36 +0200

    visualisations done

--- parent/code/visualisation/route_visualisation_image.py
+++ parent/code/visualisation/route_visualisation_image.py
@@ -4,11 +4,26 @@ import numpy.linalg as np
 import manim as m
 
 class RouteVisualisationImage(RouteVisualisationScene):
-    """Show trainroutes on map."""
+    """
+    A manim scene animating a collection of train routes on a given map
+    as an image. Inherits all RouteVisualisationScene attributes, but
+    overwrites certain methods to create image instead of video.
+    """
 
-    # Main method calling upon other methods to color route and move train
     def color_route(self, route: list, color: m.manim_colors):
+        """
+        Main route coloring method calling upon other class methods to color
+        all train routes in self.output.
 
+        Pre
+        ---
+        Route input is a list of route objects. Color is a manim color 
+        contained in self.colors.
+
+        Post
+        ---
+        Calls upon set_color() to set color of line in route. 
+        """ 
         for connection in route:
             time = connection[2]
 
@@ -21,8 +36,22 @@ class RouteVisualisationImage(RouteVisualisationScene):
             # Set color of line
             self.set_color(line, color)
 
-    # Set color depending on if already colored
     def set_color(self, line: m.Line, color: m.manim_colors):
+        """
+        Colors route a color or calls upon set_multiple_colors.
+
+        Pre
+        ---
+        Line is a line object corresponding to an existing connection in 
+        RailNL(self.map). Color is a manim color contained in self.colors.
+
+        Post
+        ---
+        Sets color of line uniformly if line is not already colored by 
+        adding color to line statically.
+        Calls upon set_multiple_color if color must be added to an already
+        colored line.
+        """
 
         # If line not yet colored just color
         if not line in self.line_colors_dict:
@@ -35,9 +64,22 @@ class RouteVisualisationImage(RouteVisualisationScene):
                 colors = self.line_colors_dict[line]
                 colors.append(color)
                 self.set_multiple_colors(line, colors)
-
-    # Set line with multiple colors 
+ 
     def set_multiple_colors(self, line: m.Line, colors: list):
+        """
+        Colors line with multiple colors using line_colors_dict.
+
+        Pre
+        ---
+        Line is a line object corresponding to an existing connection in 
+        RailNL(self.map). Color is a manim color contained in self.colors.
+
+        Post
+        ---
+        Statically creates a new line on top of old line. New line is
+        created with an additional color by creating several uniquely 
+        colored line segments.
+        """
 
         # Find start and end brutally
         starting_point = line.start
@@ -72,4 +114,7 @@ class RouteVisualisationImage(RouteVisualisationScene):
         self.add(new_line)
 
     def is_video(self):
+        """
+        Method to use in construct() to create either image or video.
+        """       
         return False
