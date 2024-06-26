3ff4b1c82d754a07a2826f2b139a157c37b9e058
tije-schut <tije.c.schut@gmail.com>
2024-06-26 16:37:47 +0200

    bby

--- parent/main3.py
+++ parent/main3.py
@@ -16,13 +16,13 @@ highest = files[-1]
 # Construct path to file from main
 path = f"autorun_hillclimber/mijn_eerste_project/solutions/{highest}"
 
-# Set manim settings 
-command = ["python3", "set_manim_settings.py", "Holland", path]
-subprocess.run(command)
-
 # Now we need to navigate to the correct working directory because we are not software engineers
 working_directory = os.getcwd()
 new_path = os.chdir(f"{working_directory}/parent/code/visualisation")
+
+# Set manim settings 
+command = ["python3", "set_manim_settings.py", "Holland", path]
+subprocess.run(command)
 
 # Visualise manim image
 command = ["manim", "-pql", "route_visualisation_image.py", "RouteVisualisationImage"]
